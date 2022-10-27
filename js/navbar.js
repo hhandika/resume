1b3604f5491ca0c80dac1bf9adeaa66e304a61fc
Heru Handika <herubiolog@gmail.com>
2022-10-27 15:21:50 -0500

    Disable unused function.

--- js/navbar.js
+++ js/navbar.js
@@ -2,3 +2,9 @@
 $(document).ready(function () {
   $("#navbar").load("navbar.html");
 });
+
+// function getCurrentPage() {
+//   var path = window.location.pathname;
+//   var page = path.split("/").pop();
+//   return page;
+// }
