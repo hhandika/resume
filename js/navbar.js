b1cded227c49c43550e13eeb07983452fe8252d2
Heru Handika <herubiolog@gmail.com>
2023-06-30 14:23:37 -0500

    Add action buttons and update cv.

--- js/navbar.js
+++ js/navbar.js
@@ -2,9 +2,3 @@
 $(document).ready(function () {
   $("#nav-bar").load("navbar.html");
 });
-
-// function getCurrentPage() {
-//   var path = window.location.pathname;
-//   var page = path.split("/").pop();
-//   return page;
-// }
